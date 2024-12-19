464348d661c7c783e05ece9a62b61f8b8bd22608
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2024-12-19 22:46:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+let openModal = document.getElementById('openModal');
+let modal = document.querySelector('.modal');
+let closeModal = document.querySelector('.close-modal');
+
+// add event listener to openModal
+openModal.addEventListener('click', function() {
+  modal.style.display = 'block';
+});
+
+// add event listener to closeModal
+closeModal.addEventListener('click', function() {
+  modal.style.display = 'none';
+});
+
+// add event listener to modal
+modal.addEventListener('click', function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+});
